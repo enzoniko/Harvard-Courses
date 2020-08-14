b6a938599ada10c7c976ff0e46c0d87efaa5a09b
Enzo <enzonsb@gmail.com>
2020-08-14 18:10:44 -0300

    pushed assignment10 GD50

---
+++ Unity Engine/enzoniko-games50-assignments-2020-x-10/Assets/Scripts/HandleWin.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Text))]
+public class HandleWin : MonoBehaviour
+{
+    public GameObject _text;
+    private Text text;
+    private bool restart = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = _text.GetComponent<Text>();
+        text.color = new Color(255, 255, 255, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       if (other.tag == "Player") 
+       {
+           text.color = new Color(255, 255, 255, 255);
+           restart = true;
+             
+       }
+    }
+
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.anyKeyDown) 
+            {
+               SceneManager.LoadScene("AssignmentScene");
+               PortalGun.orangeUsed = false;
+               PortalGun.blueUsed = false;
+            } 
+        }
+    }
+}
